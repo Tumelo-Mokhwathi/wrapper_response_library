11e68b9320d822f0c4743e2b6f5c2cded4bd3e47
https://git.afrigis.co.za/Tumelo <tumelo@afrigis.co.za>
2021-04-17 15:56:30 +0200

    new project

---
+++ ActionResult/ActionResultGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using WrapperResponse.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WrapperResponse.ActionResult
+{
+    public static class ActionResultGenerator
+    {
+        public static IActionResult ErrorResult(HttpStatusCode code, string message, string source)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return new ObjectResult(new CreateErrorResponse(code, message, source))
+            {
+                StatusCode = (int)code
+            };
+        }
+
+        public static IActionResult SuccessResult(HttpStatusCode code, object result, string source)
+        {
+            if (result == null)
+            {
+                throw new ArgumentNullException(nameof(result));
+            }
+            if (string.IsNullOrWhiteSpace(source))
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            return new JsonResult(new { code, result, source });
+        }
+    }
+}
---
+++ Response/CreateErrorResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace WrapperResponse.Response
+{
+    public class CreateErrorResponse
+    {
+        public HttpStatusCode code { get; set; }
+        public string message { get; set; }
+        public string source { get; set; }
+
+        public CreateErrorResponse(HttpStatusCode Code, string Message, string Source)
+        {
+            code = Code;
+            message = Message;
+            source = Source;
+        }
+    }
+}
